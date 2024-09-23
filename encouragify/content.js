0bc033dfcc06231cb56d9692803726726b4b7d53
Felipe Orestes <Orestefe@gmail.com>
2024-09-23 15:13:23 -0400

    Initial commit

--- encouragify/content.js
+++ encouragify/content.js
@@ -1,44 +1,30 @@
-// Wrap the script inside an IIFE to isolate its scope and avoid redeclaration errors
 (function() {
-  // Initialize state only inside the IIFE
   let state = {
     currentPage: 1,
     lastPage: 1,
-    isRunning: false,  // Initially set to false to avoid multiple runs
+    isRunning: false,
   };
 
   function triggerEncourages() {
-    console.log('Triggering encourages');
     document.querySelectorAll('.thumbs-up').forEach(el => el.click());
   }
 
   function clickNextPage() {
     document.querySelector('[class*=chevron_right]').click();
-    console.log('Clicking next page');
     state.currentPage++;
   }
 
   function startApp() {
     if (!state.isRunning) {
       const pageRange = document.querySelector('ss-paginator > span').textContent.replace(/\s/g, '');
-      console.log(pageRange);
-
       state.isRunning = true;
-      console.log('App started');
-
       [state.currentPage, state.lastPage] = pageRange.split('/').map(el => parseInt(el));
       triggerEvents();
-    } else {
-      console.warn('App is already running!');
     }
   }
 
   function triggerEvents() {
-    console.log(state.currentPage);
-    console.log(state.isRunning);
-
     if (state.currentPage < state.lastPage && state.isRunning) {
-      console.log(`Triggering events on page ${state.currentPage}`);
       triggerEncourages();
       clickNextPage();
       setTimeout(triggerEvents, 3000);
@@ -49,20 +35,18 @@
   }
 
   function stopApp() {
-    if (state.isRunning) {
-      state.isRunning = false;
-      state.currentPage = 1;
-      state.lastPage = 1;
-      console.log("App stopped.");
-    } else {
-      console.warn('App is not running, cannot stop.');
-    }
+    state.isRunning = false;
+    state.currentPage = 1;
+    state.lastPage = 1;
   }
 
-  // Automatically starts the app when injected (only if not already running)
-  startApp();
-
-  // Expose stopApp function globally, so it can be called from background.js
-  window.stopApp = stopApp;
+  // Listen for messages from the background script
+  chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    if (message.action === 'startScript') {
+      startApp();
+    } else if (message.action === 'stopScript') {
+      stopApp();
+    }
+  });
 
 })();
